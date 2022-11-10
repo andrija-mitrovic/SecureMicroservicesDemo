f53442d334d81ce74430aab95d44928742702ab1
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-11-10 08:47:27 +0100

    Refactore consume operation with IHttpClientFactory

--- Movies.Client/ApiServices/MovieApiService.cs
+++ Movies.Client/ApiServices/MovieApiService.cs
@@ -10,6 +10,13 @@ namespace Movies.Client.ApiServices
 {
     public class MovieApiService : IMovieApiService
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MovieApiService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+        }
+
         public async Task<Movie> CreateMovie(Movie movie)
         {
             throw new System.NotImplementedException();
@@ -27,39 +34,54 @@ namespace Movies.Client.ApiServices
 
         public async Task<IEnumerable<Movie>> GetMovies()
         {
-            var apiClientCredentials = new ClientCredentialsTokenRequest
-            {
-                Address = "https://localhost:5005/connect/token",
-                ClientId = "movieClient",
-                ClientSecret = "secret",
-                Scope = "movieAPI"
-            };
+            //WAY 1
+            var httpClient = _httpClientFactory.CreateClient("MovieAPIClient");
 
-            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, "/api/movies/");
 
-            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5005");
-            if (disco.IsError)
-            {
-                return null;
-            }
+            var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).ConfigureAwait(false);
 
-            var tokenResponse = await client.RequestClientCredentialsTokenAsync(apiClientCredentials);
-            if (tokenResponse.IsError)
-            {
-                return null;
-            }
-
-            var apiClient = new HttpClient();
-            apiClient.SetBearerToken(tokenResponse.AccessToken);
-
-            var response = await apiClient.GetAsync("https://localhost:5001/api/movies");
             response.EnsureSuccessStatusCode();
 
             var content = await response.Content.ReadAsStringAsync();
-
-            List<Movie> movieList = JsonConvert.DeserializeObject<List<Movie>>(content);
+            var movieList = JsonConvert.DeserializeObject<List<Movie>>(content);
 
             return movieList;
+
+            //WAY 2
+            //var apiClientCredentials = new ClientCredentialsTokenRequest
+            //{
+            //    Address = "https://localhost:5005/connect/token",
+            //    ClientId = "movieClient",
+            //    ClientSecret = "secret",
+            //    Scope = "movieAPI"
+            //};
+
+            //var client = new HttpClient();
+
+            //var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5005");
+            //if (disco.IsError)
+            //{
+            //    return null;
+            //}
+
+            //var tokenResponse = await client.RequestClientCredentialsTokenAsync(apiClientCredentials);
+            //if (tokenResponse.IsError)
+            //{
+            //    return null;
+            //}
+
+            //var apiClient = new HttpClient();
+            //apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            //var response = await apiClient.GetAsync("https://localhost:5001/api/movies");
+            //response.EnsureSuccessStatusCode();
+
+            //var content = await response.Content.ReadAsStringAsync();
+
+            //List<Movie> movieList = JsonConvert.DeserializeObject<List<Movie>>(content);
+
+            //return movieList;
         }
 
         public async Task<Movie> UpdateMovie(Movie movie)
--- Movies.Client/Startup.cs
+++ Movies.Client/Startup.cs
@@ -5,7 +5,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Net.Http.Headers;
 using Movies.Client.ApiServices;
+using Movies.Client.HttpHandlers;
+using System;
 
 namespace Movies.Client
 {
@@ -46,6 +49,15 @@ namespace Movies.Client
 
                 options.GetClaimsFromUserInfoEndpoint = true;
             });
+
+            services.AddTransient<AuthenticationDelegatingHandler>();
+
+            services.AddHttpClient("MovieAPIClient", client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:5001/");
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
+            }).AddHttpMessageHandler<AuthenticationDelegatingHandler>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

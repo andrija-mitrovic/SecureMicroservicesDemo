2d79f429d97137cfc1ad8e43498f2a26b1eb7d02
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-11-20 08:50:17 +0100

    Configure only admin page

--- Movies.Client/ApiServices/IMovieApiService.cs
+++ Movies.Client/ApiServices/IMovieApiService.cs
@@ -11,5 +11,6 @@ namespace Movies.Client.ApiServices
         Task<Movie> CreateMovie(Movie movie);
         Task<Movie> UpdateMovie(Movie movie);
         Task DeleteMovie(string id);
+        Task<UserInfoViewModel> GetUserInfo();
     }
 }
